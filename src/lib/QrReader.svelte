c0869dacf8f3b7b3294abed6bc140994f8281e9e
Ngwsky <ngwsky@gmail.com>
2022-05-23 11:00:44 +0900

    fix gh-pages deployment settings

--- src/lib/QrReader.svelte
+++ src/lib/QrReader.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import jsQR from "jsqr"
+  import jsQR from 'jsqr'
+  import qrcode from 'qrcode-generator'
 
   let canvasElement
   let videoElement
@@ -24,9 +25,9 @@
         drawline(ctx, code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58")
         drawline(ctx, code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58")
         drawline(ctx, code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58")
-        if (code.data && (qrValues.length < 1 || qrValues[0] != code.data)) {
-          qrValues = [code.data, ...qrValues]
-          console.log(code)
+        if (code.data && (qrValues.length < 1 || qrValues[0].data != code.data)) {
+          const svg = getQrSvg(code.data)
+          qrValues = [{no: qrValues.length + 1, version: code.version, data: code.data, svg: svg, img: svg2img(svg)}, ...qrValues]
         }
         animationid = requestAnimationFrame(tick)
         // cancelAnimationFrame(animationid)
@@ -47,6 +48,17 @@
     ctx.stroke()
   }
 
+  function getQrSvg(val) {
+    const qr = qrcode(0, 'Q')
+    qr.addData(val)
+    qr.make()
+    return qr.createSvgTag({cellSize:1})
+  }
+
+  function svg2img(svg) {
+    return URL.createObjectURL(new Blob([svg], {type: "image/svg+xml;charset=utf-8"}))
+  }
+
   onMount(() => {
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then((stream) => {
       videoElement.srcObject = stream
@@ -65,9 +77,26 @@
 <video bind:this={videoElement} hidden></video>
 <div class="row">
   <div class="column">
-    <ul>
-      {#each qrValues as qrValue}<li>{qrValue}</li>{/each}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>QR</th>
+          <th>V</th>
+          <th>Data</th>
+        </tr>
+    </thead>
+    <tbody>
+      {#each qrValues as qrValue}
+      <tr>
+        <td>{qrValue.no}</td>
+        <td><a download={qrValue.no + ".svg"} href={qrValue.img}>{@html qrValue.svg}</a></td>
+        <td>{qrValue.version}</td>
+        <td>{qrValue.data}</td>        
+      </tr>
+      {/each}
+    </tbody>
+    </table>
   </div>
 </div>
 
